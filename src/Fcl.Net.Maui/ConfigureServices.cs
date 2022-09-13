aedce866f3be0292a3d1a17ab0c55fda0a4de97f
Tyron Brand <44845449+tyronbrand@users.noreply.github.com>
2022-09-13 11:27:47 +0100

    create MAUI project

---
+++ src/Fcl.Net.Maui/ConfigureServices.cs
@@ -0,0 +1,66 @@
+﻿using Fcl.Net.Core;
+using Fcl.Net.Core.Config;
+using Fcl.Net.Core.Service;
+using Fcl.Net.Core.Service.Strategy;
+using Flow.Net.Sdk.Client.Http;
+using Flow.Net.Sdk.Core.Client;
+
+namespace Fcl.Net.Maui
+{
+    public static class ConfigureServices
+    {
+        public static IServiceCollection AddFclServices(this IServiceCollection services, FlowClientOptions sdkClientOptions, FclConfig fclConfig)
+        {
+            // sdk client
+            services.AddSingleton(f =>
+            {
+                return sdkClientOptions;
+            });
+            services.AddHttpClient<IFlowClient, FlowHttpClient>();
+
+            // fetch service
+            services.AddSingleton(f =>
+            {
+                var fetchServiceConfig = new FetchServiceConfig
+                {
+                    Location = fclConfig.Location
+                };
+
+                return fetchServiceConfig;
+            });
+            services.AddHttpClient<FetchService>();
+
+            // local views
+
+            // strategies
+
+            // fcl
+            services.AddSingleton(f =>
+            {
+                return fclConfig;
+            });
+
+            services.AddSingleton(f =>
+            {
+                // local views
+                var localViews = new Dictionary<FclServiceMethod, ILocalView>
+                {                    
+                };
+
+                // strategies
+                var strategies = new Dictionary<FclServiceMethod, IStrategy>
+                {                    
+                };
+
+                return new Core.Fcl(
+                    f.GetRequiredService<IFlowClient>(),
+                    f.GetRequiredService<FclConfig>(),
+                    localViews,
+                    f.GetRequiredService<FetchService>(),
+                    strategies);
+            });
+
+            return services;
+        }
+    }
+}
